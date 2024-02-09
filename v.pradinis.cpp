900955d52381fb1a56019b1a612df00313330c20
VakarisT <vakaris.tiunaitis@gmail.com>
2024-02-09 10:53:13 +0200

    Skaitymas

--- v.pradinis.cpp
+++ v.pradinis.cpp
@@ -1,5 +1,37 @@
-#include<iostream>
+#include <iostream>
+#include <iomanip>
 
-int main(){
-    std::cout << "Pajehali";
+using namespace std;
+const int N = 2;
+
+struct Stud{
+string Vard;
+string Pavard;
+int paz[];
+int egz;
+float vid;
+};
+
+void Skaityti(Stud A);
+
+int main()
+{
+    Stud A[N];
+    Skaityti(A);
+    return 0;
+}
+void Skaityti(Stud A)
+{
+    int n , s = 0;
+    cout << "Iveskite studento varda: "; cin >> A.Vard;
+    cout << "Iveskite studento pavarde: "; cin >> A.Pavard;
+    cout << "Kiek pazymiu studentas gavo: "; cin >> n;
+    cout << "Iveskite tuos pazymius: ";
+    for(int i = 0; i < n; i++)
+        {
+            cin >> A.paz[i];
+            s += A.paz[i];
+        }
+    cout << "Iveskite egzamino pazymi: "; cin >> A.egz;
+    A.vid = s / n;
 }
