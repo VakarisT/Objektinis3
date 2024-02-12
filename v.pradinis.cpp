db8addb0a016baa52465a2a1845eb12815304d89
VakarisT <vakaris.tiunaitis@gmail.com>
2024-02-12 19:49:43 +0200

    message

--- v.pradinis.cpp
+++ v.pradinis.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <algorithm>
 
 using namespace std;
 const int N = 2;
@@ -10,17 +11,28 @@ string Pavard;
 int paz[];
 int egz;
 float vid;
+double med;
 };
 
-void Skaityti(Stud A);
+void Skaityti(Stud & A);
+double Median(Stud A, int n);
 
 int main()
 {
     Stud A[N];
-    Skaityti(A);
+    for(int i = 0; i < N; i++)
+        Skaityti(A[i]);
+    cout << "Vardas  Pavarde         Galutinis(vid.) / Galutinis(med.)" << endl;
+    cout << "---------------------------------------------------------" << endl;
+    for(int j = 0; j < N; j++)
+    {
+        cout << A[j].Vard << '\t' << A[j].Pavard << '\t';
+        cout << fixed << setprecision(2) << 0.4 * A[j].vid + 0.6 * A[j].egz << '\t' << '\t' << '\t';
+        cout << fixed << setprecision(2) << 0.4 * A[j].vid + 0.6 * A[j].med << '\n';
+    }
     return 0;
 }
-void Skaityti(Stud A)
+void Skaityti(Stud & A)
 {
     int n , s = 0;
     cout << "Iveskite studento varda: "; cin >> A.Vard;
@@ -28,10 +40,20 @@ void Skaityti(Stud A)
     cout << "Kiek pazymiu studentas gavo: "; cin >> n;
     cout << "Iveskite tuos pazymius: ";
     for(int i = 0; i < n; i++)
-        {
-            cin >> A.paz[i];
-            s += A.paz[i];
-        }
+    {
+        cin >> A.paz[i];
+        s += A.paz[i];
+    }
     cout << "Iveskite egzamino pazymi: "; cin >> A.egz;
-    A.vid = s / n;
+    A.vid = (float)s / n;
+    A.med = Median(A,n);
+}
+double Median(Stud A, int n)
+{
+    double m;
+    sort(A.paz, A.paz + n);
+    if(n % 2 == 0)
+        m = (double)A.paz[n / 2];
+    m = (double)(A.paz[(n - 1) / 2] + A.paz[n / 2]) / 2.0;
+    return m;
 }
