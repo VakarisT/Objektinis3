b3272c4c3b186b789cf419d7f30565bf7e9458ef
VakarisT <vakaris.tiunaitis@gmail.com>
2024-02-12 22:43:08 +0200

    labas

--- v.pradinis.cpp
+++ v.pradinis.cpp
@@ -8,7 +8,7 @@ const int N = 2;
 struct Stud{
 string Vard;
 string Pavard;
-int paz[];
+int paz[10];
 int egz;
 float vid;
 double med;
@@ -20,7 +20,7 @@ double Median(Stud A, int n);
 int main()
 {
     Stud A[N];
-    for(int i = 0; i < N; i++)
+    for(int i = 0; i < 2; i++)
         Skaityti(A[i]);
     cout << "Vardas  Pavarde         Galutinis(vid.) / Galutinis(med.)" << endl;
     cout << "---------------------------------------------------------" << endl;
@@ -34,17 +34,24 @@ int main()
 }
 void Skaityti(Stud & A)
 {
-    int n , s = 0;
+    int n = 0 , s = 0;
     cout << "Iveskite studento varda: "; cin >> A.Vard;
     cout << "Iveskite studento pavarde: "; cin >> A.Pavard;
-    cout << "Kiek pazymiu studentas gavo: "; cin >> n;
+    {cout << "Kiek pazymiu studentas gavo: "; cin >> n;}
+    if (n == 0 || n > 100)
+        exit(EXIT_FAILURE);
+
     cout << "Iveskite tuos pazymius: ";
     for(int i = 0; i < n; i++)
     {
         cin >> A.paz[i];
+        while(A.paz[i] < 1 || A.paz[i] > 10)
+        {cout << "Iveskite tinkama pazymi: "; cin >> A.paz[i];}
         s += A.paz[i];
     }
     cout << "Iveskite egzamino pazymi: "; cin >> A.egz;
+    while(A.egz < 1 || A.egz > 10)
+        {cout << "Iveskite tinkama pazymi: "; cin >> A.egz;}
     A.vid = (float)s / n;
     A.med = Median(A,n);
 }
