9cbd54b25d5d3f4320b1ef92a004f91758a01d26
VakarisT <147090929+VakarisT@users.noreply.github.com>
2024-06-13 12:36:31 +0300

    Update person.h

--- person.h
+++ person.h
@@ -1,24 +1,36 @@
-#pragma once
-
 #ifndef PERSON_H
 #define PERSON_H
 
 #include <string>
 
+/// @class Person
+/// @brief Abstrakti žmogaus klasė
 class Person
 {
 protected:
     std::string name_, surname_;
+    /// @brief Default konstruktorius
+    Person() : name_(""), surname_("") {};
+    /// @brief Žmogaus klasės konstruktorius
+    /// @param name Vardas
+    /// @param surname Pavardė
+    Person(const std::string& name, const std::string& surname) : name_(name), surname_(surname) {};
+    /// @brief Žmogus klasės destruktorius
+    virtual ~Person() {};
 
 public:
+    /// @brief Vardo gavimas
+    /// @return Vardas
     inline std::string get_Name() const { return name_; }
+    /// @brief Pavardės gavimas
+    /// @return Pavardė
     inline std::string get_Surname() const { return surname_; }
+    /// @brief Vardo nustatymas
+    /// @param name 
     void set_Name(std::string name) { this->name_ = name; }
+    /// @brief Pavardės nustatymas
+    /// @param surname 
     void set_Surname(std::string surname) { this->surname_ = surname; }
-    virtual void whoAmI() = 0;
-    Person() : name_(""), surname_("") {};
-    Person(const std::string& name, const std::string& surname) : name_(name), surname_(surname) {};
-    virtual ~Person() {};
 };
 
 #endif
